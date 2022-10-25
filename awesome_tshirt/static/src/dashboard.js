e24f49a994c7d9c778588ddb2d75fc5870b727b2
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2022-10-25 20:47:39 +0200

    1.2 A Todo Component

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -2,14 +2,19 @@
 
 //Import the counter from new js file
 import { Counter } from "./counter/counter";
+import { Todo } from "./todo/todo";
 import { registry } from "@web/core/registry";
 
 const { Component } = owl;
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+    setup() {
+        this.todo = { id: 3, description: "buy milk", done: false };
+    }
+}
 
 //Define that this Dashboard uses that counter
-AwesomeDashboard.components = { Counter }
+AwesomeDashboard.components = { Counter, Todo };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);

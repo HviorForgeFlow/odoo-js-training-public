d0d8a1cdea562744653d32f7bef1db9a442a5051
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2022-10-25 21:01:22 +0200

    1.3 Props Validation

--- awesome_tshirt/static/src/todo/todo.js
+++ awesome_tshirt/static/src/todo/todo.js
@@ -2,6 +2,17 @@
 
 const { Component } = owl;
 
-export class Todo extends Component {}
+export class Todo extends Component {
+//    static props = {
+//        id: { type: Number },
+//        description: { type: String },
+//        done: { type: Boolean },
+//    }; Right solution but it can be done as below:
+}
 
 Todo.template = "awesome_tshirt.Todo";
+Todo.props = {
+    id: { type: Number },
+    description: { type: String },
+    done: { type: Boolean },
+};

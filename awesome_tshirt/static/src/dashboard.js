811b6f94018a3fef06ad5206b919c87268c0cbb3
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2022-10-27 18:50:07 +0200

    1.4 A list of todos: The right way

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -2,23 +2,15 @@
 
 //Import the counter from new js file
 import { Counter } from "./counter/counter";
-import { Todo } from "./todo/todo";
+import { TodoList } from "./todo_list/todo_list";
 import { registry } from "@web/core/registry";
 
 const { Component } = owl;
 
-class AwesomeDashboard extends Component {
-    setup() {
-        this.todos = [
-            { id: 3, description: "buy milk", done: false },
-            { id: 4, description: "buy eggs", done: true },
-            { id: 5, description: "buy milk", done: false }
-        ];
-    }
-}
+class AwesomeDashboard extends Component {}
 
 //Define that this Dashboard uses that counter
-AwesomeDashboard.components = { Counter, Todo };
+AwesomeDashboard.components = { Counter, TodoList };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
